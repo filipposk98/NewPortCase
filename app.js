078c546a01d4356ddbdfea3ea6d495b96ff9b0b6
filipposk98 <70683986+filipposk98@users.noreply.github.com>
2020-11-04 23:32:29 +0100

    First Step
    adjusting the parallax effect

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+//Hero parallax effect//
 $(window).scroll(function() {
     var scrollPos = $(this).scrollTop();
-    var background = $('.hero-back');
-    
-    $(".hero-back").css({
-          'background-size' : 100 - scrollPos + "%"
+    // var background = $('.hero-back');
+ 
+    // $(".hero-back").css({
+    //       'background-size' : 1400 - scrollPos  + 500
 
-         
+    //     });  
+        $(document).ready(function() {
+            var s = skrollr.init();
+          })
+       
 
     });
-    if (scrollPos < 40) {
-$(".hero-back").css({    
-'backgroud-size' : 50 - scrollPos + 'px'
-});
-}
-});
+//     if (scrollPos < 100) {
+// $(".hero-back").css({    
+// 'backgroud-size' : 150 - scrollPos + 'px'
+// });
+// }
+// });
